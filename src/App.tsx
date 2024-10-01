d3f32df12fd392a392038f893e3f3505da96d00d
Gabe Serna <gabriel.serna118@gmail.com>
2024-10-01 08:06:32 -0400

    Add Hero Section Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ButtonLG, Heading1 } from "./components/Typography";
+import { MapPin, UtensilsCrossed } from "lucide-react";
 
 function App() {
   return (
@@ -13,9 +14,32 @@ function App() {
         id="hero"
         className="w-full h-screen pt-12 lg:pt-20 flex flex-col px-12 lg:px-20"
       >
-        <Heading1 className="text-3xl 2xl:text-6xl xl:text-5xl lg:text-4xl mt-28 lg:mt-40">
+        <Heading1 className="text-3xl 2xl:text-6xl xl:text-5xl lg:text-4xl mt-32 lg:mt-48">
           We Make Food, You Make Progress
         </Heading1>
+        <div id="values" className="flex flex-col lg:flex-row w-full space-x-8 mt-4">
+          <div className="flex mt-8 items-center justify-center border-blue-500 border-2 lg:border-[3px] rounded-lg w-1/2">
+            <MapPin className="min-h-7 min-w-7 mx-3 stroke-slate-600" />
+            <input
+              type="text"
+              placeholder="Enter Delivery Address"
+              className="w-full rounded-lg py-4 focus:outline-none mr-3"
+            ></input>
+          </div>
+          <div className="flex mt-8 items-center justify-center border-blue-500 border-2 lg:border-[3px] rounded-lg min-w-max w-1/6">
+            <UtensilsCrossed className="min-h-7 min-w-7 mx-3 stroke-slate-600" />
+            <select
+              title="Select Goal"
+              defaultValue={"Maintenence"}
+              className="w-full rounded-lg py-4 focus:outline-none mr-3"
+            >
+              <option value="Maintenence">Maintenence</option>
+              <option value="Bulking">Bulking</option>
+              <option value="Cutting">Cutting</option>
+            </select>
+          </div>
+        </div>
+        <ButtonLG className="w-max mt-6">Find Plans</ButtonLG>
       </section>
       <section
         id="info"
